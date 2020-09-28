5f20068aeb96545936c210f5d35e3ee86ca1a91b
John Ward <john@johnward.net>
2020-09-28 16:16:51 +0100

    Starting UI Creating

--- src/ui/main_window.rs
+++ src/ui/main_window.rs
@@ -13,8 +13,20 @@ pub struct MainWindow {
 }
 
 impl MainWindow {
-    pub fn create_application_ui(has_gl: bool) {
+    pub fn create_application_ui(&self, has_gl: bool) {
+        let application = gtk::Application::new(Some("Pillars"), Default::default())
+            .expect("Unable to load Application");
+
+        application.connect_activate(|app| {
+            self.build_ui(app);
+        });
+    }
+
+    fn build_ui(&self, application: &gtk::Application) {
         let glade_src = include_str!("main_window.glade");
-        //let builder = Builder::
+        let builder = Builder::from_string(glade_src);
+        let window = builder
+            .get_object("Pillars")
+            .expect("Could not get Pillars Main Window");
     }
 }
